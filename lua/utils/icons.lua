9bf8c03a45ea6a901681f717208ad673f23bfcee
Kaizokage <kaizokage@duck.com>
2024-08-25 15:57:56 +0700

    upload utility icons

---
+++ lua/utils/icons.lua
@@ -0,0 +1,70 @@
+return {
+    misc = {
+        dots = "󰇘",
+    },
+
+    dap = {
+        Stopped             = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+        Breakpoint          = " ",
+        BreakpointCondition = " ",
+        BreakpointRejected  = { " ", "DiagnosticError" },
+        LogPoint            = ".>",
+    },
+
+    diagnostics = {
+        Error = " ",
+        Warn  = " ",
+        Hint  = " ",
+        Info  = " ",
+    },
+    
+    git = {
+        added    = " ", -- " "
+        modified = " ", -- " "
+        removed  = " ", -- " "
+    },
+    
+    kinds = {
+        Namespace = "󰌗 ",
+        Text = "󰉿 ",
+        Method = "󰆧 ",
+        Function = "󰆧 ",
+        Constructor = " ",
+        Field = "󰜢 ",
+        Variable = "󰀫 ",
+        Class = "󰠱 ",
+        Interface = " ",
+        Module = " ",
+        Property = "󰜢 ",
+        Unit = "󰑭 ",
+        Value = "󰎠 ",
+        Enum = " ",
+        Key = " ",
+        Keyword = "󰌋 ",
+        Snippet = " ",
+        Color = "󰏘 ",
+        File = "󰈚 ",
+        Reference = "󰈇 ",
+        Folder = "󰉋 ",
+        EnumMember = " ",
+        Constant = "󰏿 ",
+        Struct = "󰙅 ",
+        Event = " ",
+        Operator = "󰆕 ",
+        TypeParameter = "󰊄 ",
+        Table = " ",
+        Object = "󰅩 ",
+        Tag = " ",
+        Array = "[] ",
+        Boolean = " ",
+        Number = " ",
+        Null = "󰟢 ",
+        String = "󰉿 ",
+        Calendar = " ",
+        Watch = "󰥔 ",
+        Package = " ",
+        Copilot = " ",
+        Codeium = " ",
+        TabNine = " ",
+    },
+}
