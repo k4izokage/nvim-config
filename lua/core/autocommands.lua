25c10d54f539d4d1ab1f0559972718449cd9c97e
Kaizokage <kaizokage@duck.com>
2024-08-26 16:28:35 +0700

    customize cursor visibility on Alpha dashboard
    - Added auto command to hide the cursor when Alpha dashboard is ready.
    - Restored cursor visibility after leaving Alpha dashboard.

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,5 +1,8 @@
+local opt = vim.opt
+local get_hl = vim.api.nvim_get_hl
+local set_hl = vim.api.nvim_set_hl
 local autocmd = vim.api.nvim_create_autocmd
-
+--------------------------------------------------
 -- Apply new custom highlights, override hl_group
 local hlgroup = require("utils.highlights")
 
@@ -8,4 +11,31 @@ autocmd("ColorScheme", {
   callback = function()
     require("utils").set_custom_highlight(hlgroup.alpha_plugin)
   end
+})
+--------------------------------------------------
+-- Hide cursor for Alpha dashboard
+autocmd('User', {
+  pattern = 'AlphaReady',
+  callback = function()
+    -- Get current Cursor highlight group
+    local cursor_hl = get_hl(0, {name = 'Cursor', link = false})
+    -- Set blend to 100 to hide the cursor
+    cursor_hl.blend = 100
+    set_hl(0, 'Cursor', cursor_hl)
+    -- Append custom guicursor setting
+    opt.guicursor:append('a:Cursor/lCursor')
+  end,
+})
+
+-- Restore cursor after leaving Alpha dashboard
+autocmd('BufUnload', {
+  buffer = 0,
+  callback = function()
+    -- Reset the Cursor highlight group
+    local cursor_hl = get_hl(0, {name = 'Cursor', link = false})
+    cursor_hl.blend = 0
+    set_hl(0, 'Cursor', cursor_hl)
+    -- Remove custom guicursor setting
+    opt.guicursor:remove('a:Cursor/lCursor')
+  end,
 })
